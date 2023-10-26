2aa30d0e0cf49faec526407c55f3f477fb6a14aa
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-10-26 13:33:35 +0900

    4단계완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -8,16 +8,15 @@ class App {
         System.out.println("==명업 앱 ==");
         int number =1;
         while (true) {
-            //int number =1;
             System.out.print("명령) ");
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
-                System.out.printf("명언: ");
+                System.out.print("명언: ");
                 String str = scanner.nextLine();
-                System.out.printf("작가: ");
+                System.out.print("작가: ");
                 String str2 = scanner.nextLine();
                 System.out.printf("%d번 명언이 등록되었습니다%n" , number++);
 
