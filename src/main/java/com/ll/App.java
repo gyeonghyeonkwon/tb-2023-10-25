4985d72bc655c893b987b6337f364f312e7d3e9c
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-10-26 11:24:50 +0900

    3단계완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,22 +6,21 @@ class App {
 
     void run() {
 
-            System.out.println("==명업 앱 ==");
-            while (true) {
+        System.out.println("==명업 앱 ==");
+        while (true) {
             System.out.print("명령) ");
-
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
             if (cmd.equals("종료")) {
-
                 break;
-            }
-            else if (cmd.equals("등록")) {
-                System.out.println("명언: 현재를사랑하라.");
-                System.out.println("작가: 작자미상");
-                System.out.println("1번 명언이 등록되었습니다");
-            }
+            } else if (cmd.equals("등록")) {
+                System.out.printf("명언: ");
+                String str = scanner.nextLine();
+                System.out.printf("작가: ");
+                String str2 = scanner.nextLine();
+                System.out.println("1번 명언이 등록되었습니다" );
 
+            }
         }
     }
 }
