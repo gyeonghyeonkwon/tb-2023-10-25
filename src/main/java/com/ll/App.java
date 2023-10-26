39c3fc83928eb5bda9b7b51852a5db6c2a1beaea
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-10-26 17:13:39 +0900

    4단계완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -3,26 +3,24 @@ package com.ll;
 import java.util.Scanner;
 
 class App {
-
     void run() {
         int number = 0;
-        System.out.println("==명업 앱 ==");
-        while (true) {
-            System.out.print("명령) ");
-            Scanner scanner = new Scanner(System.in);
-            String cmd = scanner.nextLine();
-            if (cmd.equals("종료")) {
-                break;
-            } else if (cmd.equals("등록")) {
-
-                System.out.print("명언: ");
-                String str1 = scanner.nextLine();
-                System.out.print("작가: ");
-                String str2 = scanner.nextLine();
-                number++;
-                System.out.printf("%d번 명언이 등록되었습니다%n", number);
-            }
-
-        }
+        System.out.println("==명언 앱==");
+       while (true) {
+           Scanner scanner = new Scanner(System.in);
+           System.out.printf("명령) ");
+           String cmd = scanner.nextLine();
+           if (cmd.equals("종료")) {
+               break;
+           }
+           else if (cmd.equals("등록")){
+               System.out.printf("명언 : ");
+               String wiseSaying= scanner.nextLine();
+               System.out.printf("작가 : ");
+               String author= scanner.nextLine();
+               number ++;
+               System.out.printf("%d번 명언이 등록되었습니다.%n" , number);
+           }
+       }
     }
 }
