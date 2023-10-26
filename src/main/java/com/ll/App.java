a2786af76c66b9517fe49403a6d6691efd6f1eed
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-10-26 10:06:25 +0900

    3단계완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -19,6 +19,7 @@ class App {
             else if (cmd.equals("등록")) {
                 System.out.println("명언: 현재를사랑하라.");
                 System.out.println("작가: 작자미상");
+                System.out.println("1번 명언이 등록되었습니다");
             }
 
         }
